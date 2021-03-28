f933b88c44a9859ef0f794b16c50f39286fd23b9
Polina Romankova <pola.royn@tut.by>
2021-03-28 14:29:23 +0300

    fix comments

--- fanfics/src/Comment/Comment.js
+++ fanfics/src/Comment/Comment.js
@@ -17,21 +17,22 @@ class  Comment extends React.Component {
 
     render() {
         return (
-            <div className="form-outline comment-box border" id = "comment-form">
+            <div className="form-outline comment-box border">
                 <label>Имя:</label>
-                <input className="name form-control"
+                <input className="name form-control" id= "comment-user-name"
                 onChange={event=>{
                     this.setState({'name': event.target.value})
                 }}/>
                 <label>Комментарий:</label>
-                <textarea className="form-control comments" rows="4"
+                <textarea className="form-control comments" rows="4" id = "comment-text"
                           onChange={event=>{
                               this.setState({'text': event.target.value})
                           }}> </textarea>
                 <button className="btn btn-outline custom-button send"
                 onClick = {()=> {
+                    document.getElementById("comment-user-name").value = ""
+                    document.getElementById("comment-user-name").value = ""
                         addComment(this.state.name , this.state.text , this.props.book)
-                    document.getElementById("comment-form").reset()
                 }}>Отправить</button>
             </div>
         );
